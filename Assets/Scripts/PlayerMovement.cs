24632276585bee1bbcf614fbfa8647aca328fd76
GamerYuan <f2pplayer0801@gmail.com>
2023-02-11 17:00:56 +0800

    better movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,23 +17,27 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    
+
     // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.D)) || (Input.GetKeyDown(KeyCode.A) && Input.GetKey(KeyCode.D)))
+        {
+            float rotation = (transform.eulerAngles.z + 90) * Mathf.Deg2Rad;
+            rb.AddForce(new Vector2(speed * Mathf.Cos(rotation), speed * Mathf.Sin(rotation)), ForceMode2D.Impulse);
+        }
+    }
     void FixedUpdate()
     {
         float horizontalMove = Input.GetAxisRaw("Horizontal");
 
         if (horizontalMove > 0) {
-            rb.AddTorque(-degrees * Mathf.Deg2Rad * rb.inertia, ForceMode2D.Force);
+            rb.AddTorque(-degrees * Mathf.Deg2Rad * rb.inertia, ForceMode2D.Impulse);
         } 
         else if (horizontalMove < 0)
         {
-            rb.AddTorque(degrees * Mathf.Deg2Rad * rb.inertia, ForceMode2D.Force);
+            rb.AddTorque(degrees * Mathf.Deg2Rad * rb.inertia, ForceMode2D.Impulse);
         }
 
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) {
-            float rotation = (transform.eulerAngles.z + 90) * Mathf.Deg2Rad;
-            rb.AddForce(new Vector2(speed * Mathf.Cos(rotation), speed * Mathf.Sin(rotation)), ForceMode2D.Force);
-        }
     }
 }
