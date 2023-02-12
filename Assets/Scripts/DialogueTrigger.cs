41b068e3dff1d0410094dd692702dd3598bb8a36
GamerYuan <f2pplayer0801@gmail.com>
2023-02-12 16:15:16 +0800

    final

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -19,7 +19,7 @@ public class DialogueTrigger : MonoBehaviour
         "Press the 'A' key to turn left and the 'D' key to turn right.", 
         "Pressing both 'A' and 'D' at the same time will boost you forward. Note that dashing consumes rats.",
         "Grow your rat army by keeping everyone well fed, though remember that you can't eat more than your own weight in cheese at once!", 
-        "Finally, make sure not to get eaten by cats and try not to knock into the dead bodies of your brethren. Reach 100,000 rats to pull the cheese moon towards you and end your rat homelessness.", };
+        "Finally, make sure not to get eaten by cats and try not to knock into the dead bodies of your brethren. Reach 1,000,000 rats to pull the cheese moon towards you and end your rat homelessness.", };
 
         dialogue.setSentences(controls);
         dialogue.setName("(=^-w-^=)");
